c83b52b1bd506a98104f9c6fb6b06ea9edfd465f
Ricardo Signes <rjbs@cpan.org>
2019-05-01 13:35:30 -0400

    Async: correctly delegate requests to async client

--- lib/JMAP/Tester/UA/Async.pm
+++ lib/JMAP/Tester/UA/Async.pm
@@ -43,22 +43,17 @@ sub request {
   my $logger = $tester->_logger;
 
   my $log_method = "log_" . ($log_type // 'jmap') . '_request';
-  $self->ua->set_my_handler(request_send => sub {
-    my ($req) = @_;
-    $logger->$log_method({
-      ($log_extra ? %$log_extra : ()),
-      http_request => $req,
-    });
-    return;
-  });
 
-  my $http_res = $self->lwp->request($post);
+#  $self->ua->set_my_handler(request_send => sub {
+#    my ($req) = @_;
+#    $logger->$log_method({
+#      ($log_extra ? %$log_extra : ()),
+#      http_request => $req,
+#    });
+#    return;
+#  });
 
-  # Clear our handler, or it will get called for
-  # any http request our ua makes!
-  $self->ua->set_my_handler(request_send => undef);
-
-  return Future->done($http_res);
+  return $self->client->do_request(request => $req);
 }
 
 no Moo;
